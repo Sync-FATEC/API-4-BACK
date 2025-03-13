2369cc583c0f9a10d3e1418cb764bbe7d393107a
João Gabriel <joaoggbs62@gmail.com>
2025-03-13 13:50:59 -0300

    feat: Mudando o banco de dados de MySql para PostgreSQL

--- src/infrastructure/database/data-source.ts
+++ src/infrastructure/database/data-source.ts
@@ -4,12 +4,12 @@ import { DataSource } from 'typeorm';
 import { User } from '../../domain/entities/User';
 
 export const AppDataSource = new DataSource({
-    type: 'mysql',
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 3306,
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'api42025',
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: false,
     entities: [User],
