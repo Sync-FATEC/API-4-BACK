330490a0c816724a57efbada3aff0d40ba5cd20f
Kaue-Francisco <kauesantos.francisco@gmail.com>
2025-03-23 00:25:12 -0300

    feat: Implement DeleteTypeParameter use case and controller

--- src/web/routes/typeParameter.routes.ts
+++ src/web/routes/typeParameter.routes.ts
@@ -4,6 +4,8 @@ import TypeParemeterRepository from "../../infrastructure/repositories/TypeParam
 import { CreateTypeParameterUseCase } from "../../application/use-cases/typeParameter/CreateTypeParameterUseCase";
 import UpdateTypeParameterUseCase from "../../application/use-cases/typeParameter/UpdateTypeParameterUseCase";
 import { UpdateTypeParameterController } from "../controllers/typeParameters/UpdateTypeParameterController";
+import DeleteTypeParameterUseCase from "../../application/use-cases/typeParameter/DeleteTypeParameterUsecase";
+import { DeleteTypeParameterController } from "../controllers/typeParameters/DeleteTypeParameterController";
 
 const typeParameterRoutes = Router();
 const typeParametersRepository = new TypeParemeterRepository();
@@ -16,9 +18,15 @@ const createController = new CreateTypeParametersController(createTypeParameterU
 const updateTypeParameterUseCase = new UpdateTypeParameterUseCase(typeParametersRepository);
 const updateController = new UpdateTypeParameterController(updateTypeParameterUseCase);
 
+// Delete
+const deleteTypeParameterUseCase = new DeleteTypeParameterUseCase(typeParametersRepository);
+const deleteController = new DeleteTypeParameterController(deleteTypeParameterUseCase);
+
 // Routes
 typeParameterRoutes.post('/create', (req, res) => createController.handle(req, res));
 
 typeParameterRoutes.put('/update', (req, res) => updateController.handle(req, res));
+
+typeParameterRoutes.delete('/delete/:id', (req, res) => deleteController.handle(req, res));
 
 export { typeParameterRoutes }
