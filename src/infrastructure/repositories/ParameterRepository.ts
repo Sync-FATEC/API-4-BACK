12f5781acd2a94f18d958590e528e46cca1b34ab
yokotaerik <yokota082003@gmail.com>
2025-03-25 18:35:17 -0300

    feat: adicionando listagem de parametro com DTO personalizado

--- src/infrastructure/repositories/ParameterRepository.ts
+++ src/infrastructure/repositories/ParameterRepository.ts
@@ -2,34 +2,48 @@ import { Repository } from "typeorm";
 import { AppDataSource } from "../database/data-source";
 import Parameter from "../../domain/models/agregates/Parameter/Parameter";
 import { IParameterRepository } from "../../domain/interfaces/repositories/IParameterRepository";
+import ListParameterDTO from "../../web/dtos/parameter/ListParameterDTO";
 
 export class ParameterRepository implements IParameterRepository {
-    private repository: Repository<Parameter>;
+  private repository: Repository<Parameter>;
 
-    constructor() {
-        this.repository = AppDataSource.getRepository(Parameter);
-    }
+  constructor() {
+    this.repository = AppDataSource.getRepository(Parameter);
+  }
 
-    async create(parameter: Partial<Parameter>): Promise<Parameter> {
-        const newParameter = this.repository.create(parameter);
-        return await this.repository.save(newParameter);
-    }
+  async create(parameter: Partial<Parameter>): Promise<Parameter> {
+    const newParameter = this.repository.create(parameter);
+    return await this.repository.save(newParameter);
+  }
 
-    async delete(id: string): Promise<boolean> {
-        const result = await this.repository.delete(id);
-        return result.affected === 1;
-    }
+  async delete(id: string): Promise<boolean> {
+    const result = await this.repository.delete(id);
+    return result.affected === 1;
+  }
 
-    async update(id: string, parameter: Partial<Parameter>): Promise<Parameter> {
-        await this.repository.update(id, parameter);
-        return await this.repository.findOneByOrFail({ id });
-    }
+  async update(id: string, parameter: Partial<Parameter>): Promise<Parameter> {
+    await this.repository.update(id, parameter);
+    return await this.repository.findOneByOrFail({ id });
+  }
 
-    async list(): Promise<Parameter[]> {
-        return await this.repository.find();
-    }
+  async list(): Promise<Parameter[]> {
+    return await this.repository.find();
+  }
 
-    async findById(id: string): Promise<Parameter | null> {
-        return await this.repository.findOneBy({ id });
-    }
+  async listDTO(): Promise<ListParameterDTO[]> {
+    const parameters = await this.repository.find({
+      relations: ["idStation", "idTypeParameter"],
+    });
+    return parameters.map(
+      (parameter) =>
+        new ListParameterDTO(
+          parameter.id,
+          parameter.idStation.name + " - " + parameter.idTypeParameter.name
+        )
+    );
+  }
+
+  async findById(id: string): Promise<Parameter | null> {
+    return await this.repository.findOneBy({ id });
+  }
 }
--- src/web/routes/parameter.routes.ts
+++ src/web/routes/parameter.routes.ts
@@ -4,6 +4,8 @@ import { CreateParameterUseCase } from "../../application/use-cases/parameter/Cr
 import { ParameterRepository } from "../../infrastructure/repositories/ParameterRepository";
 import { limiter } from "../../infrastructure/middlewares/limiter";
 import { ensureAuthenticated } from "../../infrastructure/middlewares/ensureAuthenticated";
+import { ListParameterUseCase } from "../../application/use-cases/parameter/ListParameterUseCase";
+import { ListParameterController } from "../controllers/parameter/ListParameterController";
 
 const parameterRoutes = Router();
 const parameterRepository = new ParameterRepository();
@@ -12,7 +14,12 @@ const parameterRepository = new ParameterRepository();
 const createParameterUseCase = new CreateParameterUseCase(parameterRepository);
 const createController = new CreateParameterController(createParameterUseCase);
 
+// List
+const listParameterUseCase = new ListParameterUseCase(parameterRepository);
+const listController = new ListParameterController(listParameterUseCase);
+
 // Routes
 parameterRoutes.post('/create', limiter, ensureAuthenticated, (req, res) => createController.handle(req, res));
+parameterRoutes.get('/list', limiter, ensureAuthenticated, (req, res) => listController.handle(req, res));
 
 export { parameterRoutes }
