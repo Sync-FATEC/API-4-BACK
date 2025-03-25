da97064fc5bb91ca01534b9bb32e9f5a142c7d07
Kaue-Francisco <kauesantos.francisco@gmail.com>
2025-03-25 19:24:24 -0300

    feat: Implement DeleteParameter use case and controller for parameter deletion functionality

---
+++ src/web/controllers/parameter/DeleteParameterController.ts
@@ -0,0 +1,25 @@
+import { Request } from "express";
+import DeleteParameterUseCase from "../../../application/use-cases/parameter/DeleteParameterUseCase";
+
+export class DeleteParameterController {
+    constructor(private deleteParameterUseCase: DeleteParameterUseCase) {}
+
+    async handle(request: Request, res): Promise<Response> {
+        try {
+            const { id } = request.params;
+
+            if (!id) {
+                return res.sendError('Parâmetro não encontrado ou inexistente', 400);
+            }
+
+            await this.deleteParameterUseCase.execute(id);
+
+            return res.sendSuccess('Parâmetro deletado com sucesso', 200);
+        } catch (error) {
+            return res.sendError(
+                error instanceof Error ? error.message : 'Erro inesperado',
+                400
+            );
+        }
+    }
+}
