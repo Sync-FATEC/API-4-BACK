6291d46110c10486df65e7ac263b64e998f44949
Kaue-Francisco <kauesantos.francisco@gmail.com>
2025-03-25 19:24:30 -0300

    feat: Implement UpdateParameter use case and controller for parameter update functionality

---
+++ src/application/use-cases/parameter/UpdateParameterUseCase.ts
@@ -0,0 +1,19 @@
+import { IParameterRepository } from "../../../domain/interfaces/repositories/IParameterRepository";
+import Parameter from "../../../domain/models/agregates/Parameter/Parameter";
+import UpdateParameterDTO from "../../../web/dtos/parameter/UpdateParameterDTO";
+
+export default class UpdateParameterUseCase {
+    constructor(private parameterRepository: IParameterRepository) {}
+
+    async execute(parameterData: UpdateParameterDTO) {
+        const parameter = await this.parameterRepository.findById(parameterData.getId());
+        if (!parameter) {
+            throw new Error('Parâmetro não encontrado');
+        }
+
+        return await this.parameterRepository.update(parameterData.getId(), {
+            idTypeParameter: parameterData.getIdTypeParameter(),
+            idStation: parameterData.getIdStation(),
+        });
+    }
+}
