20a53094a21d020852dc4e5612f5c01fd9b29e83
João Gabriel <joaoggbs62@gmail.com>
2025-03-27 18:45:49 -0300

    Revert "Update src/application/use-cases/receiverJson/receiverJsonUseCase.ts"
    This reverts commit 77d4eb6e2b1f8643d06977ba540133d73a13483b.

--- src/application/use-cases/receiverJson/receiverJsonUseCase.ts
+++ src/application/use-cases/receiverJson/receiverJsonUseCase.ts
@@ -51,7 +51,7 @@ export default class ReceiverJsonUseCase {
                 if (this.shouldTriggerAlert(measure.value, typeAlert.comparisonOperator, typeAlert.value)) {
                     const alertData: RegisterAlertDTO = {
                         date: unixtime,
-                        typeAlertId: typeAlert.id,
+                        typeAlerdId: typeAlert.id,
                         measureId: measure.id
                     };
                     await registerAlertUseCase.execute(alertData);
