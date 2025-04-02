83ae8d393a8cc6c14d3401be48a83998d028ff68
João Gabriel <joaoggbs62@gmail.com>
2025-04-02 12:16:31 -0300

    feat: update global setup to start and stop server on port 3000 for tests

--- tests/setup/globalSetup.ts
+++ tests/setup/globalSetup.ts
@@ -3,18 +3,22 @@ import { Server } from 'http';
 
 let server: Server;
 
-export default async function globalSetup() {
-    server = app.listen(5001, () => {
-        console.log('Server started on port 5001');
+async function startServer() {
+    server = app.listen(3000, () => {
+        console.log('Server started on port 3000');
     });
-
-    // Retorna uma função de limpeza que será chamada após todos os testes
-    return async () => {
-        await new Promise<void>((resolve) => {
-            server.close(() => {
-                console.log('Server stopped');
-                resolve();
-            });
-        });
-    };
+    return server;
 }
+
+async function stopServer() {
+    return new Promise<void>((resolve) => {
+        server.close(() => {
+            resolve();
+        });
+    });
+}
+
+export default async function globalSetup() {
+    await startServer();
+    await stopServer(); 
+};
