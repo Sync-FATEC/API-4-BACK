45d7a7fc361d8c02b9458265518eab31a24137dc
João Gabriel <joaoggbs62@gmail.com>
2025-04-07 08:49:12 -0300

    fix: arrumando ci, para parar o servidor

--- tests/setup/globalSetup.ts
+++ tests/setup/globalSetup.ts
@@ -1,24 +1,13 @@
-import { app } from '../../src/server';
+import { app, startServer as startAppServer } from '../../src/server';
 import { Server } from 'http';
 
-let server: Server;
+export let server: Server;
 
 async function startServer() {
-    server = app.listen(3000, () => {
-        console.log('Server started on port 3000');
-    });
+    server = await startAppServer(String(3000));
     return server;
-}
-
-async function stopServer() {
-    return new Promise<void>((resolve) => {
-        server.close(() => {
-            resolve();
-        });
-    });
 }
 
 export default async function globalSetup() {
     await startServer();
-    await stopServer(); 
-};
+}
